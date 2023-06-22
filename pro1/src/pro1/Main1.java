4b8d6b6f7025beb5fcffe52837d4bb957c80d60d
SriranjaniT <sriranjanit9@gmail.com>
2023-06-22 10:43:43 +0530

    first program java

---
+++ pro1/src/pro1/Main1.java
@@ -0,0 +1,19 @@
+package pro1;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num=-8;
+        if (num>=0)
+        {
+        	if (num==0)
+        		System.out.println("The number is zero");
+        	else
+        		System.out.println("The number is positive");
+        }
+        else
+        	System.out.println("The number is negative");
+	}
+
+}
