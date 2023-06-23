82a42ba782692c120c4fb708171cf5f7465c44d7
SriranjaniT <sriranjanit9@gmail.com>
2023-06-23 09:10:28 +0530

    second program

---
+++ pro2/src/pro2/Main2.java
@@ -0,0 +1,12 @@
+package pro2;
+public class Main2 {
+	public static void main(String[] args) {
+        int num=6;
+        if (num%2==0)
+        	System.out.println("The number is even");
+        else
+        	System.out.println("The number is odd");
+        
+	}
+
+}
