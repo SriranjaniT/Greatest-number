f9078d3ec947734bd91441378de7b89b55f095e0
SriranjaniT <sriranjanit9@gmail.com>
2023-06-23 09:48:32 +0530

    fourth program

---
+++ pro4/src/pro4/Main1.java
@@ -0,0 +1,17 @@
+package pro4;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int num1=3;
+         int num2=7;
+         if (num1==num2)
+        	 System.out.println("Both are equal");
+         else if (num1>=num2)
+        	 System.out.println("Num1 is greatest");
+         else
+        	 System.out.println("Num2 is greatest");
+	}
+
+}
